d0a5391f2545cc27f3e3f982ddf053da023a7301
Scipion <zapador50@gmail.com>
2023-11-02 11:49:15 +0100

    Bump up versions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import 'dom4';
 import '@webcomponents/custom-elements';
 import 'mutation-observer';
 
+import 'todomvc-common/base.css';
+import 'todomvc-app-css/index.css';
+
 import TodoAppElement from './todo-app';
 import TodoInputElement from './todo-input';
 import TodoListElement from './todo-list';
